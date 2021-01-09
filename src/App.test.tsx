093e1c607cef82567c893cc4b7e943ec65431010
davidyang2149 <davidyang2149@gmail.com>
2021-01-09 14:36:50 +0900

    Set development environment
    Img Link Generator Frontend
    
    - Initialize file(index & app).
    - Add ph-pages.

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+
+describe('App', () => {
+  it('renders the home page', () => {
+    const { container } = render((
+      <App />
+    ));
+
+    expect(container).toHaveTextContent('로그인');
+  });
+});
